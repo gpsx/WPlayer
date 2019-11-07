032cce97bd7fac6ce296ed54ae1601cda871ff65
Your Name <you@example.com>
2019-11-06 23:06:31 -0200

    gráfico da home funcionando

--- app/controllers/home-gamer.js
+++ app/controllers/home-gamer.js
@@ -7,18 +7,22 @@ module.exports = {
         return result
     },
     getTop5Games: async (STEAM_ID)=>{
-        var result = await db('VW_TOP5_ACTIVE').where({ STEAM_ID })
+        var result = await db('VW_TOP_ACTIVE').where({ STEAM_ID }).limit(5).orderBy('TOTAL_HOURS', 'desc')
         console.log(result);
         return result
     },
     getLastHardware: async (CUSTUMER_KEY)=>{
-        var result = await db('VW_TOP5_ACTIVE').where({ CUSTUMER_KEY })
-        console.log(result);
-        return result
+        var gameDetails = await db('VW_GAME_DETAILS').where({GAME_ID : '291550'})
+        var gameDLC = await db('VW_DLCS').where({GAME_ID : '291550'})
+        console.log(gameDetails, gameDLC);
+        
+        //var result = await db('VW_TOP_ACTIVE').where({ CUSTUMER_KEY })
+        //console.log(result);
+        //return result
     },
     getRecomendations: async (STEAM_ID)=>{
-        var result = await db('VW_TOP5_ACTIVE').where({ STEAM_ID })
-        console.log(result);
-        return result
-    },
+        //var result = await db('VW_TOP_ACTIVE').where({ STEAM_ID })
+        //console.log(result);
+        //return result
+    }
 };
--- app/routes/gamer/home.js
+++ app/routes/gamer/home.js
@@ -5,10 +5,11 @@ module.exports = function (app) {
 	});
 	app.get('/gamer/home', async (req, res) => {
 		const user = req.session.user
-		const top5 = controller.getTop5Games(user.PLAYER_ID)
-		const twoWeeks = controller.get2WeeksGames(user.PLAYER_ID)
-		const hardwareInfo = controller.getLastHardware(user.CUSTUMER_KEY)
-		const recomendations = controller.getRecomendations(user.PLAYER_ID)
+		const top5 = await controller.getTop5Games(user.PLAYER_ID)
+		const twoWeeks = await controller.get2WeeksGames(user.PLAYER_ID)
+		const hardwareInfo = await controller.getLastHardware(user.CUSTUMER_KEY)
+		const recomendations = await controller.getRecomendations(user.PLAYER_ID)
+		console.log(top5, twoWeeks, hardwareInfo, recomendations)
 		res.render('gamer/home', { top5, twoWeeks, hardwareInfo, recomendations });
 	});
 }
