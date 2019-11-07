527e3664e3425c9506e3b4a337437a00b44aa0d2
Guilherme de Paula Santos <guilherme.paula@TIT-1523.local>
2019-11-07 15:11:57 -0300

    recomendations ready

--- app/controllers/home-gamer.js
+++ app/controllers/home-gamer.js
@@ -1,28 +1,34 @@
 const db = require('../../config/db')
-
+const { removeSomeChar } = require('../utils/index')
+var dataNormalizer = (games)=>{
+    for (const index in games) {
+        games[index].APP_NAME = removeSomeChar(games[index].APP_NAME, "\'", "")
+    }
+    return games
+}
 module.exports = {
     get2WeeksGames: async (STEAM_ID) =>{
-        var result = await db('VW_RECENTLY_ACTIVE').where({ STEAM_ID })
-        console.log(result);
+        var result = dataNormalizer(await db('VW_RECENTLY_ACTIVE').where({ STEAM_ID }).orderBy('WEEKS_HOURS', 'desc'))
         return result
     },
     getTop5Games: async (STEAM_ID)=>{
-        var result = await db('VW_TOP_ACTIVE').where({ STEAM_ID }).limit(5).orderBy('TOTAL_HOURS', 'desc')
-        console.log(result);
+        var result = dataNormalizer(await db('VW_TOP_ACTIVE').where({ STEAM_ID }).limit(5).orderBy('TOTAL_HOURS', 'desc'))
         return result
     },
     getLastHardware: async (CUSTUMER_KEY)=>{
         var gameDetails = await db('VW_GAME_DETAILS').where({GAME_ID : '291550'})
-        var gameDLC = await db('VW_DLCS').where({GAME_ID : '291550'})
-        console.log(gameDetails, gameDLC);
+        //var gameDLC = await db('VW_DLCS').where({GAME_ID : '291550'})
+        console.log(gameDetails);
         
         //var result = await db('VW_TOP_ACTIVE').where({ CUSTUMER_KEY })
         //console.log(result);
         //return result
     },
     getRecomendations: async (STEAM_ID)=>{
-        //var result = await db('VW_TOP_ACTIVE').where({ STEAM_ID })
-        //console.log(result);
-        //return result
+        var result = await db('APPS').limit(6)
+        for (const index in result) {
+            result[index].APP_GENRES = result[index].APP_GENRES.split(" - ")
+        }
+        return result
     }
 };
