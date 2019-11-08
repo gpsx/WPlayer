1202aed96f2202e6192b715490b605b22c342d8c
Your Name <you@example.com>
2019-11-08 20:44:32 -0200

    Finalização da Tela gameDetails

--- app/routes/gamer/game-detail.js
+++ app/routes/gamer/game-detail.js
@@ -2,7 +2,9 @@ const controller = require('../../controllers/game-detail')
 
 module.exports = function (app) {
 	app.get('/gamer/game/detail/:GAME_ID', async (req, res) => {
-        await controller.getInformation(req.params.GAME_ID)
-		res.render('gamer/game-detail', { err: {} });
+        var information = await controller.getInformation(req.params.GAME_ID)
+        console.log(information);
+        
+		    res.render('gamer/game-detail', { information });
     });
 }
