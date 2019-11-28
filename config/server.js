3ec37b7b07f2d4dbbcbdf67d59abe571c98e196e
Guilherme de Paula Santos <guilherme.paula@TIT-1523.local>
2019-11-28 15:12:56 -0300

    port 3000

--- config/server.js
+++ config/server.js
@@ -22,7 +22,7 @@ var corpController = require('../app/controllers/home-corp')
 var machineController = require('../app/controllers/machine-detail')
 var utils = require('../app/utils/index')
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3000
 
 /* setar as variáveis 'view engine' e 'views' do express */
 app.set('view engine', 'ejs');
