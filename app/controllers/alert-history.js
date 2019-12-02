758fcf57e8dc8a8e9f5671a979ab3699e2d94fdd
ZoeiroVader <vortexgamedevs@gmail.com>
2019-12-02 00:11:31 -0300

    corp history

--- app/controllers/alert-history.js
+++ app/controllers/alert-history.js
@@ -1,10 +1,10 @@
 const db = require('../../config/db')
 
 module.exports = {
-   getAlerts: async ()=>{
-       var alerts= await db('APPS').where('APP_PRICE','>','100.00').limit(50)
-       console.log(alerts)
-       return alerts
+   getStates: async (CUSTOMER_ID)=>{
+       var states = await db('MACHINE_PROCESS').where({CUSTOMER_ID})
+       console.log(states)
+       return states
    }
 
 };
